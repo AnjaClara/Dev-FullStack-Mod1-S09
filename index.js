9a0548e90cc2640285480a6a8424265d66ec8816
AnjaClara <anaclaraguimaraes1@gmail.com>
2023-03-31 22:16:19 -0300

    EX4

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const connection = require('./src/database');
+const Place = require('./src/models/place');
 const app = express();
 
 app.use(express.json())
+// //EX1-3
+// const places = [];
 
-const places = [];
-
-connection.authenticate()
+connection.authenticate();
+connection.sync();
 console.log("Connection has been established successfully");
 
 app.get('/', (request, response) => {
@@ -14,9 +16,31 @@ app.get('/', (request, response) => {
   response.json({mensagem: "Olá!"})
 })
 
+//EX4
+app.get('/places', async(request, response)=>{
 
-app.post('/places', (request, response)=>{
-  console.log(places)
+  const newPlace = await Place.findAll();
+  response.status(201).json(newPlace); 
+  
+});
+
+// //EX1-3
+// app.post('/places', (request, response)=>{
+//   console.log(places)
+//   const place = {
+//     name: request.body.name,
+//     telefone: request.body.telefone,
+//     opening_hours: request.body.opening_hours,
+//     description: request.body.description,
+//     latitude: request.body.latitude,
+//     longitude: request.body.longitude
+//   }
+//   places.push(place);
+//   response.status(201).json(place); 
+// });
+
+//EX4
+app.post('/places', async(request, response)=>{
   const place = {
     name: request.body.name,
     telefone: request.body.telefone,
@@ -25,8 +49,8 @@ app.post('/places', (request, response)=>{
     latitude: request.body.latitude,
     longitude: request.body.longitude
   }
-  places.push(place);
-  response.status(201).json(place); 
+  const newplace = await Place.create(place);
+  response.status(201).json(newplace); 
 });
 
 
